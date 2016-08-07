3701da2dd4274e8b43e85c1ff7bab351a1e67db5
Hawkurane <hawkurane@gmail.com>
2016-08-07 23:33:31 +0200

    Add files via upload
    old template for references

---
+++ old/epilation.php
@@ -0,0 +1,228 @@
+<?php
+include "entete.php";
+include "h1nav.php";
+error_reporting(E_ALL);
+echo entete("Institut de beauté Loan");
+echo headernav("Institut de beauté Loan");
+?>
+
+
+  <div class='contenu'>
+      <h2> À la cire jetable et traditionnel au miel <h3>
+      <!--__________________________Sourcils_______________________--> 
+      
+        <div class='categorie' id='Sourcils'>
+        
+            <!--1/-->
+                <div class='article' id='Sourcils'>
+                    <h2> Sourcils </h2>
+                        <p> </p>                                        
+                </div>                    
+        
+        </div>
+
+
+
+
+      <!--_______________Lèvres, menton_____________________-->
+      
+        <div class='categorie' id='Lèvres, menton'>
+        
+          <!--1/-->
+        
+              <div class='article' id='Lèvres, menton'>
+                  <h2> Lèvres, menton </h2>
+                    <p></p><!-- PAS DE DESCRIPTION -->
+              </div>
+        
+          
+                    
+       </div>
+
+      <!--_______________Visage Complet_____________________-->
+      
+        <div class='categorie' id='Visage Complet'>
+        
+          <!--1/-->
+              <div class='article' id='Visage Complet'>
+                  <h2> Visage Complet </h2>
+                    <p> </p><!-- PAS DE DESCRIPTION -->
+              </div>                  
+                            
+       </div>
+
+      <!--____________________Aisselles_________________-->
+
+        <div class='categorie' id='Aisselles'>
+
+      <!--1/-->
+      <div class='article' id='Aisselles'>
+        <h2> Aisselles </h2>
+        <p>  </p> <!-- PAS DE DESCRIPTION -->
+      </div>
+    </div>
+
+
+      <!--____________________Demi-bras___________________________-->
+
+        <div class='categorie' id='Demi-bras'>
+
+        <!--1/-->
+        <div class='article' id='Demi-bras'>
+          <h2> Demi-bras </h2>
+          <p> </p> <!-- PAS DE DESCRIPTION -->
+        </div>
+      </div>
+
+
+      <!--____________________Bras__________________________-->
+
+        <div class='categorie' id='Bras'>
+
+          <!--1/-->
+          <div class='article' id='Bras'>
+            <h2> Bras </h2>
+            <p> </p> <!-- PAS DE DESCRIPTION -->
+          </div>
+
+        </div>
+
+        <!--____________________Maillot simple__________________________-->
+
+        <div class='categorie' id='Maillot simple'>
+
+          <!--1/-->
+          <div class='article' id='Maillot simple'>
+            <h2> Maillot simple </h2>
+            <p> </p> <!-- PAS DE DESCRIPTION -->
+          </div>
+
+        </div>
+
+      <!--____________________Brésilien__________________________-->
+
+        <div class='categorie' id='Brésilien'>
+
+        <!--1/-->
+        <div class='article' id='Brésilien'>
+          <h2> Brésiliene </h2>
+          <p>  </p><!-- PAS DE DESCRIPTION -->
+        </div>
+
+      </div>
+
+      <!--____________________Semi-intégral__________________________-->
+
+        <div class='categorie' id='Semi-intégral'>
+
+        <!--1/-->
+        <div class='article' id='Semi-intégral'>
+          <h2> Semi-intégral </h2>
+          <p>  </p> <!-- PAS DE DESCRIPTION -->
+        </div>
+
+      </div>
+
+    <!--____________________Intégral__________________________-->
+
+        <div class='categorie' id='Intégral'>
+
+      <!--1/-->
+      <div class='article' id='Intégral'>
+        <h2> Intégral </h2>
+        <p>  </p> <!-- PAS DE DESCRIPTION -->
+      </div>
+
+    </div>
+
+    <!--____________________Demi-jambes_________________________-->
+
+        <div class='categorie' id='Demi-jambes'>
+
+      <!--1/-->
+      <div class='article' id='Demi-jambes'>
+        <h2> Demi-jambes </h2>
+        <p>  </p> <!-- PAS DE DESCRIPTION -->
+      </div>
+
+    </div>
+
+    <!--____________________Jambes entières_________________________-->
+
+        <div class='categorie' id='Jambes entières'>
+
+      <!--1/-->
+      <div class='article' id='Jambes entières'>
+        <h2> Jambes entières </h2>
+        <p>  </p> <!-- PAS DE DESCRIPTION -->
+      </div>
+
+    </div>
+
+    <!--____________________Cuisses_________________________-->
+
+        <div class='categorie' id='Cuisses'>
+
+      <!--1/-->
+      <div class='article' id='Cuisses'>
+        <h2> Cuisses </h2>
+        <p>  </p> <!-- PAS DE DESCRIPTION -->
+      </div>
+
+    </div>
+
+    <!--____________Epilation avec produit longue durée_____________-->
+
+        <div class='categorie' id='Epilation avec produit longue durée'>
+
+      <!--1/-->
+      
+        <h2> Epilation avec produit longue durée, application du produit anti repousse des poils </h2>
+        
+
+    <!--____________________Forfaits Epilations_____________________-->
+
+        <div class='categorie' id='Forfaits Epilations'>
+
+        <!--1/-->
+          <div class='article' id='A-JE-Maillot Brésilien'>
+            <h2> A-JE-Maillot Brésilien </h2>
+            <p>  </p> <!-- PAS DE DESCRIPTION -->
+          </div>
+      
+
+        <!--2/-->
+        <div class='article' id='A-JE-Maillot Intégral'>
+          <h2> A-JE-Maillot Intégral </h2>
+          <p>  </p> <!-- PAS DE DESCRIPTION -->
+        </div>
+
+        <!--3/-->
+        <div class='article' id='A-1/2-Maillot Brésilien'>
+          <h2> A-1/2-Maillot Brésilien </h2>
+          <p>  </p> <!-- PAS DE DESCRIPTION -->
+        </div>
+
+        <!--4/-->
+        <div class='article' id='A-1/2-Maillot Intégral'>
+          <h2> A-1/2-Maillot Intégral </h2>  
+          <p>  </p> <!-- PAS DE DESCRIPTION -->
+        </div>
+
+
+
+
+    </div>
+      
+     
+  
+  </div>
+
+    
+
+  
+
+
+</body>
+
+</html>
