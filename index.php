527b7526ebd8e5610250dde4f4cffca295867c9e
Hawkurane <hawkurane@gmail.com>
2016-08-08 04:30:28 +0200

    Section Soins visage updated
    Kappa please fix css T.T

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
                         <a class="page-scroll" href="#page-top"></a>
                     </li>
                     <li>
-                        <a class="page-scroll" href="#soins">Soins</a>
+                        <a class="page-scroll" href="#soins-visage">Soins</a>
                     </li>
                     <li>
                         <a class="page-scroll" href="#tarifs">Tarifs</a>
@@ -51,25 +51,202 @@
     </nav>
 
     <!-- Intro Section -->
+
     <section id="intro" class="intro-section">
+        <a class="btn btn-default page-scroll" href="#about">Scroll to bottom !</a>
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1>Lorem Ipsum</h1>
-                    <p>Ciliciam vero, quae Cydno amni exultat, Tarsus nobilitat, urbs perspicabilis hanc condidisse Perseus memoratur, Iovis filius et Danaes, vel certe ex Aethiopia profectus Sandan quidam nomine vir opulentus et nobilis et Anazarbus auctoris vocabulum referens, et Mopsuestia vatis illius domicilium Mopsi, quem a conmilitio Argonautarum cum aureo vellere direpto redirent, errore abstractum delatumque ad Africae litus mors repentina consumpsit, et ex eo cespite punico tecti manes eius heroici dolorum varietati medentur plerumque sospitales. Batnae municipium in Anthemusia conditum Macedonum manu priscorum ab Euphrate flumine brevi spatio disparatur, refertum mercatoribus opulentis, ubi annua sollemnitate prope Septembris initium mensis ad nundinas magna promiscuae fortunae convenit multitudo ad commercanda quae Indi mittunt et Seres aliaque plurima.</p>
-                    <a class="btn btn-default page-scroll" href="#about">Scroll to bottom !</a>
+                    <h1>Institut de beauté Loan</h1>
                 </div>
             </div>
+            <div class="row">
+                <div class="col-lg-6">
+                    <p>Adresse: 26 Rue de la Bruyère, 75009 Paris
+                        <br /> Métro 12 - Trinité ou Saint Georges
+                        <br /> Téléphone: 01 48 74 98 61
+                        <br /> Email : contact@institutbeauteloan.com</p>
+                </div>
+                <div class="col-lg-6">
+                    <p>
+                        Horaires:
+                        <br />
+                        <br /> Lundi fermé
+                        <br />
+                        <!--Insérer un "horaire aujourd'hui" (script php/js)-->
+                        Mardi à Samedi ouvert, de 9:30 à 18:30
+                        <br />
+                    </p>
+                </div>
+            </div>
+
+        </div>
         </div>
     </section>
 
     <!-- About Section -->
-    <section id="soins" class="soins-section">
+    <section id="soins-visage" class="soins-visage-section">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1>Soins Section</h1>
+                    <h1>Soins du visage</h1>
                 </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-3">
+                    <h2>Beauté Expert</h2>
+                    <div class="row">
+                        <div class="col-lg-3">
+                            <h3>Catio Vital</h3>
+                            <p>Soin beauté personnalisé, nettoyage traitant et en profondeur.</p>
+                            <ol>
+                                <li>Nettoyage profond par effet "sauna".</li>
+                                <li>Double ionisation des actifs en profondeur.</li>
+                                <li>Modelage "détente" aux huiles essentielles.</li>
+                            </ol>
+                        </div>
+                        <div class="col-lg-3">
+                            <h3>Catio Lift</h3>
+                            <p>Méthode de rajeunissement des traits du visage basée sur la stimulation des muscles. Haute technologie de luxe avec l'appareil performante.</p>
+                        </div>
+                        <div class="col-lg-3">
+                            <h3>Double Catio Vital Jeunesse</h3>
+                            <p>Nettoyage en profondeur, anti-ride et anti-temps. Retrouver la jeunesse de la peau.</p>
+                        </div>
+                        <div class="col-lg-3">
+                            <h3>Eye-Repair</h3>
+                            <p>Soin pour le contour des yeux. Anti-rides, anti-poches et anti-cernes.</p>
+                        </div>
+                        
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <h2>Beauté Essentielle</h2>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <h3>Beauté Express</h3>
+                            <p>Soin coup d'éclat, effet bonne mine.</p>
+                        </div>
+                        <div class="col-lg-6">
+                            <h3>Beauté Aromatique</h3>
+                            <p>Soin personnalisé aux huiles essentielles.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <h2>Beauté Lifting</h2>
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <h3>Beauté Lifting</h3>
+                            <p>Soin lifting et raffermissant au procollagène.</p>
+                        </div>
+                        <div class="col-lg-4">
+                            <h3>Peel & Lift</h3>
+                                <p>Soin rénovateur effet lifting-éclat. Anti-taches brunes aux acides de fruits.</p>
+                        </div>
+                        <div class="col-lg-4">
+                            <h3>Kobido</h3>
+                            <p>Stimuler l'ensemble de la circulation énergétique du visage, du cou et du décolleté.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <h2>Drainage Lympho-Energétique</h2>
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <h3>Drainage lympho-énergétique du visage</h3>
+                            <p>Dégonfler, rendre les boutons moins enflés.</p>
+                        </div>
+                        <div class="col-lg-4">
+                            <h3>Soins Ayurvedique</h3>
+                            <p>Détente profonde.</p>
+                        </div>
+                        <div class="col-lg-4">
+                            <h3>Cou</h3>
+                            <p>Retrouver la jeunesse, l'ovale du visage, dégraissage et anti "double-menton".</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row"> 
+                <div class="col-lg-3">
+                    <h2>Programme sur mesure 4 semaines</h2>
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <h3>Eclat Energie</h3>
+                            <p>Eclat Energie - 4 Soins - BA/PL/DL/BA</p>
+                        </div>
+                        <div class="col-lg-4">
+                            <h3>Eclaircissement</h3>
+                            <p>4 soins - PL/CL/PL/CL</p>
+                        </div>
+                        <div class="col-lg-4">
+                            <h3>Jeunesse Anti-tâches</h3>
+                            <p>4 soins - PL/PL/C/BL</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <h3>Pureté Matité</h3>
+                            <p>4 Soins - C/PL/PL/PL</p>
+                        </div>
+                        <div class="col-lg-offset-4 col-lg-4">
+                            <h3>Lift Fermeté</h3>
+                            <p>4 soins - PL/CL/PL/CL</p>
+                        </div>
+                    </div>    
+                </div>
+                <div class="col-lg-3">
+                    <h2>Maquilalge</h2>
+                    <div class="row">
+                        <div class="col-lg-3">
+                            <h3>Maquillage Jour</h3>
+                            <p>...</p>
+                        </div>
+                        <div class="col-lg-3">
+                            <h3>Maquillage Soir</h3>
+                            <p>Cocktail avec un soin fixateur</p>
+                        </div>
+                        <div class="col-lg-3">
+                            <h3>Maquillage Mariée</h3>
+                            <p>(Avec 1 essai offert) Atténuer les imperfections, mettre en valeur... </br>
+                                Avec un soin éclat du visage, beauté des mains, pied ou forfait + maquillage
+                            </p>
+                        </div>
+                        <div class="col-lg-3">
+                            <h3>Cours d'auto maquillage</h3>
+                            <p>Cours d'auto maquillagé personnalisé et conseils produits</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <h2>Teinture Sourcils/Cils</h2>
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <h3>Teinture Cils</h3>
+                            <p>...</p>
+                        </div>
+                        <div class="col-lg-4">
+                            <h3>Teinture Sourcils</h3>
+                            <p>....</p>
+                        </div>
+                        <div class="col-lg-4">
+                            <h3>Teinture Sourcils & Cils</h3>
+                            <p>...</p>
+                        </div>
+                        
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <h2>Réhaussement de cils</h2>
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <h3>Réhaussement de cils</h3>
+                            <p>Retrouver des cils plus longs, plus courbés pour un regard de biche et surtout sur de vrai cils, points forts garder le réhaussement des cils pendant deux mois.</p>
+                        </div>
+                    </div>    
+                </div>
+                
             </div>
         </div>
     </section>
